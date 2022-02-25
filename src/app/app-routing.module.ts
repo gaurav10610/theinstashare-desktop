5cf423298bf568b963c56ff3e1265d2bb41222e5
Gaurav Kumar Yadav <gaurav10610@gmail.com>
2022-02-25 15:06:58 +0530

    added file transfer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { TalkWindowComponent } from './talk-window/talk-window.component';
 import { GroupChatWindowComponent } from './group-chat-window/group-chat-window.component';
 import { AppDashboardComponent } from './app-dashboard/app-dashboard.component';
+import { FileTransferWindowComponent } from './file-transfer-window/file-transfer-window.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'app', component: AppDashboardComponent },
   { path: 'talk', component: TalkWindowComponent },
-  { path: 'group-chat', component: GroupChatWindowComponent }
+  { path: 'group-chat', component: GroupChatWindowComponent },
+  { path: 'file-transfer', component: FileTransferWindowComponent }
 ];
 
 @NgModule({
